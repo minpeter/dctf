d78e2a480a4245d05cbaac50c86271557de0b53b
Woonggi Min <minpeter@friendli.ai>
2023-12-14 15:33:41 +0900

    Update eslint configuration and package dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,25 +8,5 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: [],
-  rules: {
-    'no-multiple-empty-lines': [
-      'error',
-      {
-        max: 1,
-        maxEOF: 0,
-        maxBOF: 0
-      }
-    ],
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: 'block-like', next: 'export' }
-    ],
-    'no-void': [
-      'error',
-      {
-        allowAsStatement: true
-      }
-    ]
-  }
+  plugins: []
 }
--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,4 @@
 const path = require('path')
-const glob = require('glob')
-const assert = require('assert').strict
-
-const PurgecssPlugin = require('purgecss-webpack-plugin')
 
 export default (config, env, helpers) => {
   if (env.production) {
@@ -23,26 +19,6 @@ export default (config, env, helpers) => {
     path.resolve(__dirname, 'client/lib/loaders')
   )
 
-  const {
-    rule: { options: babelConfig }
-  } = helpers.getLoadersByName(config, 'babel-loader')[0]
-  babelConfig.plugins.push('transform-export-extensions')
-
-  // The webpack base config has minicssextractplugin already loaded
-  config.plugins.push(
-    new PurgecssPlugin({
-      paths: glob.sync(env.source('**/*'), { nodir: true }),
-
-      // PurgeCSS does not correctly correctly recognize selectors of the form
-      // `[class*=" btn-"], [class^="btn-"]` that Cirrus uses extensively; it
-      // also does not recognize the comparison in the attribute selector and
-      // only picks up on `class`, so we have to whitelist all `class`
-      // selectors as we cannot target only the `btn-` selectors that are being
-      // improperly removed.
-      whitelistPatternsChildren: [/class.*/]
-    })
-  )
-
   // Remove .svg from preconfigured webpack file-loader
   ;['file-loader', 'url-loader']
     .flatMap((name) => helpers.getLoadersByName(config, name))
@@ -53,53 +29,6 @@ export default (config, env, helpers) => {
 
   config.module.rules.push({
     test: /\.svg$/,
-    loader: 'svg-sprite-loader',
-    options: {
-      runtimeGenerator: require.resolve(
-        './client/lib/svg-icon-component-generator'
-      ),
-      runtimeOptions: {
-        iconModule: './components/icon'
-      }
-    }
+    loader: 'preact-svg-loader'
   })
-
-  const SizePluginWrapper = helpers.getPluginsByName(config, 'SizePlugin')[0]
-  if (SizePluginWrapper !== undefined) {
-    SizePluginWrapper.plugin.options = {
-      ...SizePluginWrapper.plugin.options,
-      stripHash: (filename) => {
-        const regex = /\.[0-9a-f]+\.((esm\.)?js|css)/
-        const match = filename.match(regex)
-        if (match === null) {
-          return filename
-        }
-        return filename.slice(0, match.index) + '.' + match[1]
-      }
-    }
-  }
-
-  if (!env.production) {
-    const HtmlWebpackPluginsWrappers = helpers.getPluginsByName(
-      config,
-      'HtmlWebpackPlugin'
-    )
-    for (const HtmlWebpackPluginWrapper of HtmlWebpackPluginsWrappers) {
-      const options = HtmlWebpackPluginWrapper.plugin.options
-      const loaderMatch = options.template.match(/^!!ejs-loader!(.*)$/)
-      assert(loaderMatch !== null)
-
-      options.template = `!!ejs-loader!mustache-config-loader!${loaderMatch[1]}`
-    }
-  }
-
-  const CopyPluginWrapper = helpers.getPluginsByName(config, 'CopyPlugin')[0]
-  if (CopyPluginWrapper !== undefined) {
-    const plugin = CopyPluginWrapper.plugin
-    plugin.patterns = plugin.patterns.filter(({ from }) => {
-      return !/node_modules[\\/]preact-cli[\\/]lib[\\/]resources[\\/](manifest.json|icon.png)$/.test(
-        from
-      )
-    })
-  }
 }
