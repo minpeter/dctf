2cac40a36774ff3f06dbf2843244969e101dd5a1
Woonggi Min <minpeter@friendli.ai>
2023-12-14 16:00:04 +0900

    Remove mustache-config-loader and .eslintrc.js, and update package.json and preact.config.js

--- client/lib/loaders/mustache-config-loader.js
+++
@@ -1,9 +0,0 @@
-const mustache = require('mustache')
-const config = require('../../config')
-
-module.exports = function (source) {
-  return mustache.render(source, {
-    jsonConfig: JSON.stringify(config),
-    config
-  })
-}
--- preact.config.js
+++ preact.config.js
@@ -1,24 +1,4 @@
-const path = require('path')
-
 export default (config, env, helpers) => {
-  if (env.production) {
-    // Disable sourcemaps
-    config.devtool = false
-  } else {
-    config.devServer.proxy = [
-      {
-        path: '/api/**',
-        target: process.env.API_URL || 'http://localhost:3000',
-        changeOrigin: true,
-        changeHost: true
-      }
-    ]
-  }
-
-  config.resolveLoader.modules.unshift(
-    path.resolve(__dirname, 'client/lib/loaders')
-  )
-
   // Remove .svg from preconfigured webpack file-loader
   ;['file-loader', 'url-loader']
     .flatMap((name) => helpers.getLoadersByName(config, name))
