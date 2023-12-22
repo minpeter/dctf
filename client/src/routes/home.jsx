51d5cf72975d08695d264ad8436a65171a83533c
Woonggi Min <minpeter@friendli.ai>
2023-12-22 15:10:43 +0900

    Update dependencies and refactor JSS implementation

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -2,23 +2,19 @@ import { Component } from "preact";
 import config from "../config";
 
 import Markdown from "../components/markdown";
-import withStyles from "../components/jss";
 
-export default withStyles(
-  {},
-  class Home extends Component {
-    componentDidMount() {
-      document.title = config.ctfName;
-    }
-
-    render({ classes }) {
-      return (
-        <div class="row u-center">
-          <div class={`col-6 ${classes.content}`}>
-            <Markdown content={config.homeContent} />
-          </div>
-        </div>
-      );
-    }
+export default class Home extends Component {
+  componentDidMount() {
+    document.title = config.ctfName;
   }
-);
+
+  render() {
+    return (
+      <div class="row u-center">
+        <div class="col-6">
+          <Markdown content={config.homeContent} />
+        </div>
+      </div>
+    );
+  }
+}
--- client/src/routes/profile.jsx
+++ client/src/routes/profile.jsx
@@ -1,7 +1,6 @@
 import { useState, useCallback, useEffect } from "preact/hooks";
 import { memo } from "preact/compat";
 import config from "../config";
-import withStyles from "../components/jss";
 
 import {
   privateProfile,
@@ -29,74 +28,68 @@ import Ctftime from "../icons/ctftime.svg";
 import useRecaptcha, { RecaptchaLegalNotice } from "../components/recaptcha";
 
 const SummaryCard = memo(
-  withStyles(
-    {},
-    ({
-      name,
-      score,
-      division,
-      divisionPlace,
-      globalPlace,
-      ctftimeId,
-      classes,
-      isPrivate,
-    }) => (
-      <div class="card">
-        <div class="content p-4 w-80">
-          <div class={classes.wrapper}>
-            <h5
-              class={`title ${
-                isPrivate ? classes.privateHeader : classes.publicHeader
-              }`}
-              title={name}
+  ({
+    name,
+    score,
+    division,
+    divisionPlace,
+    globalPlace,
+    ctftimeId,
+    isPrivate,
+  }) => (
+    <div class="card">
+      <div class="content p-4 w-80">
+        <div>
+          <h5
+            class={`title ${isPrivate ? "privateHeader" : "publicHeader"}`}
+            title={name}
+          >
+            {name}
+          </h5>
+          {ctftimeId && (
+            <a
+              href={`https://ctftime.org/team/${ctftimeId}`}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              {name}
-            </h5>
-            {ctftimeId && (
-              <a
-                href={`https://ctftime.org/team/${ctftimeId}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Ctftime style="height: 20px;" />
-              </a>
-            )}
-          </div>
-          <div class="action-bar">
-            <p>
-              <span class={`icon ${classes.icon}`}>
-                <img src={Trophy} />
-              </span>
-              {score === 0 ? "No points earned" : `${score} total points`}
-            </p>
-            <p>
-              <span class={`icon ${classes.icon}`}>
-                <img src={Rank} />
-              </span>
-              {score === 0
-                ? "Unranked"
-                : `${divisionPlace} in the ${division} division`}
-            </p>
-            <p>
-              <span class={`icon ${classes.icon}`}>
-                <img src={Rank} />
-              </span>
-              {score === 0 ? "Unranked" : `${globalPlace} across all teams`}
-            </p>
-            <p>
-              <span class={`icon ${classes.icon}`}>
-                <img src={AddressBook} />
-              </span>
-              {division} division
-            </p>
-          </div>
+              <Ctftime style="height: 20px;" />
+            </a>
+          )}
+        </div>
+        <div class="action-bar">
+          <p>
+            <span class={`icon  `}>
+              <img src={Trophy} />
+            </span>
+            {score === 0 ? "No points earned" : `${score} total points`}
+          </p>
+          <p>
+            <span class={`icon  `}>
+              <img src={Rank} />
+            </span>
+            {score === 0
+              ? "Unranked"
+              : `${divisionPlace} in the ${division} division`}
+          </p>
+          <p>
+            <span class={`icon  `}>
+              <img src={Rank} />
+            </span>
+            {score === 0 ? "Unranked" : `${globalPlace} across all teams`}
+          </p>
+          <p>
+            <span class={`icon  `}>
+              <img src={AddressBook} />
+            </span>
+            {division} division
+          </p>
         </div>
       </div>
-    )
+    </div>
   )
 );
 
-const TeamCodeCard = withStyles({}, ({ teamToken, classes }) => {
+const TeamCodeCard = ({ teamToken }) => {
   const { toast } = useToast();
 
   const tokenUrl = `${location.origin}/login?token=${encodeURIComponent(
@@ -126,7 +119,7 @@ const TeamCodeCard = withStyles({}, ({ teamToken, classes }) => {
 
         <button
           onClick={onCopyClick}
-          class={`${classes.btn} btn-info`}
+          class={`  btn-info`}
           name="btn"
           value="submit"
           type="submit"
@@ -148,178 +141,167 @@ const TeamCodeCard = withStyles({}, ({ teamToken, classes }) => {
       </div>
     </div>
   );
-});
+};
 
-const UpdateCard = withStyles(
-  {},
-  ({
-    name: oldName,
-    email: oldEmail,
-    divisionId: oldDivision,
-    allowedDivisions,
-    onUpdate,
-    classes,
-  }) => {
-    const { toast } = useToast();
-    const requestRecaptchaCode = useRecaptcha("setEmail");
+const UpdateCard = ({
+  name: oldName,
+  email: oldEmail,
+  divisionId: oldDivision,
+  allowedDivisions,
+  onUpdate,
+}) => {
+  const { toast } = useToast();
+  const requestRecaptchaCode = useRecaptcha("setEmail");
 
-    const [name, setName] = useState(oldName);
-    const handleSetName = useCallback((e) => setName(e.target.value), []);
+  const [name, setName] = useState(oldName);
+  const handleSetName = useCallback((e) => setName(e.target.value), []);
 
-    const [email, setEmail] = useState(oldEmail);
-    const handleSetEmail = useCallback((e) => setEmail(e.target.value), []);
+  const [email, setEmail] = useState(oldEmail);
+  const handleSetEmail = useCallback((e) => setEmail(e.target.value), []);
 
-    const [division, setDivision] = useState(oldDivision);
-    const handleSetDivision = useCallback(
-      (e) => setDivision(e.target.value),
-      []
-    );
+  const [division, setDivision] = useState(oldDivision);
+  const handleSetDivision = useCallback((e) => setDivision(e.target.value), []);
 
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-    const doUpdate = useCallback(
-      async (e) => {
-        e.preventDefault();
+  const doUpdate = useCallback(
+    async (e) => {
+      e.preventDefault();
 
-        let updated = false;
+      let updated = false;
 
-        if (name !== oldName || division !== oldDivision) {
-          updated = true;
+      if (name !== oldName || division !== oldDivision) {
+        updated = true;
 
+        setIsButtonDisabled(true);
+        const { error, data } = await updateAccount({
+          name: oldName === name ? undefined : name,
+          division: oldDivision === division ? undefined : division,
+        });
+        setIsButtonDisabled(false);
+
+        if (error !== undefined) {
+          toast({ body: error, type: "error" });
+          return;
+        }
+
+        toast({ body: "Profile updated" });
+
+        onUpdate({
+          name: data.user.name,
+          divisionId: data.user.division,
+        });
+      }
+
+      if (email !== oldEmail) {
+        updated = true;
+
+        let error, data;
+        if (email === "") {
           setIsButtonDisabled(true);
-          const { error, data } = await updateAccount({
-            name: oldName === name ? undefined : name,
-            division: oldDivision === division ? undefined : division,
-          });
-          setIsButtonDisabled(false);
-
-          if (error !== undefined) {
-            toast({ body: error, type: "error" });
-            return;
-          }
-
-          toast({ body: "Profile updated" });
-
-          onUpdate({
-            name: data.user.name,
-            divisionId: data.user.division,
-          });
+          ({ error, data } = await deleteEmail());
+        } else {
+          const recaptchaCode = await requestRecaptchaCode?.();
+          setIsButtonDisabled(true);
+          ({ error, data } = await updateEmail({
+            email,
+            recaptchaCode,
+          }));
         }
 
-        if (email !== oldEmail) {
-          updated = true;
+        setIsButtonDisabled(false);
 
-          let error, data;
-          if (email === "") {
-            setIsButtonDisabled(true);
-            ({ error, data } = await deleteEmail());
-          } else {
-            const recaptchaCode = await requestRecaptchaCode?.();
-            setIsButtonDisabled(true);
-            ({ error, data } = await updateEmail({
-              email,
-              recaptchaCode,
-            }));
-          }
-
-          setIsButtonDisabled(false);
-
-          if (error !== undefined) {
-            toast({ body: error, type: "error" });
-            return;
-          }
-
-          toast({ body: data });
-          onUpdate({ email });
+        if (error !== undefined) {
+          toast({ body: error, type: "error" });
+          return;
         }
 
-        if (!updated) {
-          toast({ body: "Nothing to update!" });
-        }
-      },
-      [
-        name,
-        email,
-        division,
-        oldName,
-        oldEmail,
-        oldDivision,
-        onUpdate,
-        toast,
-        requestRecaptchaCode,
-      ]
-    );
+        toast({ body: data });
+        onUpdate({ email });
+      }
 
-    return (
-      <div class="card">
-        <div class="content p-4 w-80">
-          <p>Update Information</p>
-          <p class="font-thin m-0">
-            This will change how your team appears on the scoreboard. You may
-            only change your team's name once every 10 minutes.
-          </p>
-          <div class="row u-center">
-            <Form
-              class={`col-12 ${classes.form}`}
-              onSubmit={doUpdate}
-              disabled={isButtonDisabled}
-              buttonText="Update"
+      if (!updated) {
+        toast({ body: "Nothing to update!" });
+      }
+    },
+    [
+      name,
+      email,
+      division,
+      oldName,
+      oldEmail,
+      oldDivision,
+      onUpdate,
+      toast,
+      requestRecaptchaCode,
+    ]
+  );
+
+  return (
+    <div class="card">
+      <div class="content p-4 w-80">
+        <p>Update Information</p>
+        <p class="font-thin m-0">
+          This will change how your team appears on the scoreboard. You may only
+          change your team's name once every 10 minutes.
+        </p>
+        <div class="row u-center">
+          <Form
+            class={`col-12  `}
+            onSubmit={doUpdate}
+            disabled={isButtonDisabled}
+            buttonText="Update"
+          >
+            <input
+              required
+              autocomplete="username"
+              autocorrect="off"
+              maxLength="64"
+              minLength="2"
+              icon={<img src={UserCircle} />}
+              name="name"
+              placeholder="Team Name"
+              type="text"
+              value={name}
+              onChange={handleSetName}
+            />
+            <input
+              autocomplete="email"
+              autocorrect="off"
+              icon={<img src={EnvelopeOpen} />}
+              name="email"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={handleSetEmail}
+            />
+            <select
+              icon={<img src={AddressBook} />}
+              class={`select  `}
+              name="division"
+              value={division}
+              onChange={handleSetDivision}
             >
-              <input
-                required
-                autocomplete="username"
-                autocorrect="off"
-                maxLength="64"
-                minLength="2"
-                icon={<img src={UserCircle} />}
-                name="name"
-                placeholder="Team Name"
-                type="text"
-                value={name}
-                onChange={handleSetName}
-              />
-              <input
-                autocomplete="email"
-                autocorrect="off"
-                icon={<img src={EnvelopeOpen} />}
-                name="email"
-                placeholder="Email"
-                type="email"
-                value={email}
-                onChange={handleSetEmail}
-              />
-              <select
-                icon={<img src={AddressBook} />}
-                class={`select ${classes.divisionSelect}`}
-                name="division"
-                value={division}
-                onChange={handleSetDivision}
-              >
-                <option value="" disabled>
-                  Division
-                </option>
-                {allowedDivisions.map((code) => {
-                  return (
-                    <option key={code} value={code}>
-                      {config.divisions[code]}
-                    </option>
-                  );
-                })}
-              </select>
-            </Form>
-            {requestRecaptchaCode && (
-              <div class={classes.recaptchaLegalNotice}>
-                <RecaptchaLegalNotice />
-              </div>
-            )}
-          </div>
+              <option value="" disabled>
+                Division
+              </option>
+              {allowedDivisions.map((code) => {
+                return (
+                  <option key={code} value={code}>
+                    {config.divisions[code]}
+                  </option>
+                );
+              })}
+            </select>
+          </Form>
+          {requestRecaptchaCode && <RecaptchaLegalNotice />}
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
-const Profile = ({ uuid, classes }) => {
+const Profile = ({ uuid }) => {
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
   const [data, setData] = useState({});
@@ -387,7 +369,7 @@ const Profile = ({ uuid, classes }) => {
     return (
       <div class="row u-center">
         <div class="col-4">
-          <div class={`card ${classes.errorCard}`}>
+          <div class={`card  `}>
             <div class="content p-4 w-80">
               <p class="title">There was an error</p>
               <p class="font-thin">{error}</p>
@@ -399,9 +381,9 @@ const Profile = ({ uuid, classes }) => {
   }
 
   return (
-    <div class={classes.root}>
+    <div>
       {isPrivate && (
-        <div class={classes.privateCol}>
+        <div>
           <TeamCodeCard {...{ teamToken }} />
           <UpdateCard
             {...{
@@ -417,7 +399,7 @@ const Profile = ({ uuid, classes }) => {
           )}
         </div>
       )}
-      <div class={classes.col}>
+      <div>
         <SummaryCard
           {...{
             name,
@@ -440,4 +422,4 @@ const Profile = ({ uuid, classes }) => {
   );
 };
 
-export default withStyles({}, Profile);
+export default Profile;
